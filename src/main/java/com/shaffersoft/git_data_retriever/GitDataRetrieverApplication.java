63eea4a9ad5197f2b6b1683c27f517a63e363536
Joel Shaffer <joelshaffer734@gmail.com>
2025-02-20 22:41:00 -0500

    Initial Commit

---
+++ src/main/java/com/shaffersoft/git_data_retriever/GitDataRetrieverApplication.java
@@ -0,0 +1,13 @@
+package com.shaffersoft.git_data_retriever;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitDataRetrieverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitDataRetrieverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shaffersoft/git_data_retriever/GitDataRetrieverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shaffersoft.git_data_retriever;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitDataRetrieverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
